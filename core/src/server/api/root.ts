ebb6083f5b5204bd1196a285730c0964c1950dd1
aksdes12111 <aksdes1211@gmail.com>
2023-07-24 21:42:53 -0400

    added the simple working of app using probabilty calculator functions, (no graph algo included in this

--- core/src/server/api/root.ts
+++ core/src/server/api/root.ts
@@ -1,13 +1,13 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import {SoreThroatsService } from "~/server/api/routers/SoreThroatService"; 
 import { createTRPCRouter } from "~/server/api/trpc";
 
-/**
+/*
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  sourThroats: SoreThroatsService,
 });
 
 // export type definition of API
---
+++ core/src/server/api/routers/SoreThroatService.ts
@@ -0,0 +1,60 @@
+// core/src/server/api/services/SoreThroatsService.ts
+
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
+import { calculatePneumoniaProbability, calculateAsthmaProbability } from '../utils/probabilityCalculations';
+
+export const SoreThroatsService = createTRPCRouter({
+  diagnose: publicProcedure
+    .input(
+      z.object({
+        age: z.number(),
+        gender: z.string(),
+        existingMedicalConditions: z.string(),
+        hasFever: z.boolean(),
+        feverSeverity: z.number(),
+        hasCough: z.boolean(),
+        coughDescription: z.string(),
+        hasFatigue: z.boolean(),
+        hasShortnessOfBreath: z.boolean(),
+        hasChestPain: z.boolean(),
+        hasSputum: z.boolean(),
+        sputumDescription: z.string(),
+        hasWheezing: z.boolean(),
+        troubleSleeping: z.boolean(),
+        respiratoryExposure: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const pneumoniaProbability = calculatePneumoniaProbability(input);
+      const asthmaProbability = calculateAsthmaProbability(input);
+
+      const threshold = 0.5;
+
+      let diagnosis = '';
+      let response = '';
+
+      if (pneumoniaProbability >= threshold && asthmaProbability >= threshold) {
+        diagnosis = 'Both Pneumonia and Asthma';
+        response =
+          'Based on your symptoms, there is a high probability of having both Pneumonia and Asthma. It is essential to consult a healthcare professional for a detailed evaluation and treatment.';
+      } else if (pneumoniaProbability >= threshold) {
+        diagnosis = 'Pneumonia';
+        response =
+          'Based on your symptoms, there is a high probability of having Pneumonia. It is crucial to seek immediate medical attention for a proper diagnosis and treatment.';
+      } else if (asthmaProbability >= threshold) {
+        diagnosis = 'Asthma';
+        response =
+          'Based on your symptoms, there is a high probability of having Asthma. It is important to consult a healthcare professional to manage and control your symptoms effectively.';
+      } else {
+        diagnosis = 'No Respiratory Condition';
+        response =
+          'Based on your symptoms, there is no significant indication of a respiratory condition. However, if your symptoms persist or worsen, it is advisable to seek medical advice.';
+      }
+
+      return {
+        diagnosis,
+        response,
+      };
+    }),
+});
