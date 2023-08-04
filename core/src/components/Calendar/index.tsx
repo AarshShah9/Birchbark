bb468384cc22cbc293ef819702cab17eb73462d8
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2023-08-03 23:27:50 -0500

    Calendar working SSR

--- core/src/components/Calendar/index.tsx
+++ core/src/components/Calendar/index.tsx
@@ -1,11 +1,8 @@
+// "use client";
+import React from 'react';
 import { Inject, ScheduleComponent, Day, Week, Month, Agenda } from '@syncfusion/ej2-react-schedule';
 
-interface Props {
-    event1: string;
-    event2: string;
-}
-
-const Calendar: React.FC<Props> = ({event1, event2}) => { 
+const Calendar: React.FC = () => { 
     return (
         <div>
             <ScheduleComponent>
--- core/src/pages/CustomPages/CalendarPage/index.tsx
+++ core/src/pages/CustomPages/CalendarPage/index.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import Head from 'next/head';
 import Layout from "~/components/Layout";
 import Calendar from "~/components/Calendar";
+import CalendarWrapper from "~/components/CalendarWrapper";
 import dynamic from 'next/dynamic';
-
-const DynamicScheduler = dynamic(() => import('~/components/Calendar/index'), {
-    ssr: false, // Ensure this component is not loaded during server-side rendering
-});
 
 const IndexPage: React.FC = () => {
     return (
         <Layout>
             <main>
-                <Calendar event1="Event 1" event2="Event 2" />
+                {/* <Calendar /> */}
+                <CalendarWrapper />
             </main>
         </Layout>
     );
