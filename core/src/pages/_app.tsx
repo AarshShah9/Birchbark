ea53afba39cce5f6d404803823121838f0da26fa
Aarsh.Shah <AShah@Calfrac.com>
2023-08-07 16:26:27 -0600

    Added public route

--- core/src/pages/_app.tsx
+++ core/src/pages/_app.tsx
@@ -1,66 +1,70 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { resolveValue, Toaster } from "react-hot-toast";
-import { Inter, Karla } from "next/font/google";
-import { ColorModeProvider, ColorModeScript } from "@chakra-ui/color-mode";
+import type {AppProps} from "next/app";
+import {resolveValue, Toaster} from "react-hot-toast";
+import {Inter, Karla} from "next/font/google";
+import {ColorModeProvider, ColorModeScript} from "@chakra-ui/color-mode";
+import {ClerkProvider} from "@clerk/nextjs";
 
 const inter = Inter({
-  weight: ["500", "600", "700"],
-  subsets: ["latin"],
-  display: "block",
-  variable: "--font-inter",
+    weight: ["500", "600", "700"],
+    subsets: ["latin"],
+    display: "block",
+    variable: "--font-inter",
 });
 
 const karla = Karla({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  display: "block",
-  variable: "--font-karla",
+    weight: ["400", "700"],
+    subsets: ["latin"],
+    display: "block",
+    variable: "--font-karla",
 });
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={`${karla.variable} ${inter.variable} font-sans`}>
-      <style jsx global>{`
-        html {
-          font-family: ${karla.style.fontFamily};
-        }
-        #headlessui-portal-root {
-          font-family: ${inter.style.fontFamily};
-        }
-      `}</style>
-      <ColorModeProvider>
-        <ColorModeScript
-          initialColorMode="system"
-          key="chakra-ui-no-flash"
-          storageKey="chakra-ui-color-mode"
-        />
-        <Component {...pageProps} />
-        <Toaster
-          containerStyle={{
-            bottom: 40,
-            left: 20,
-            right: 20,
-          }}
-          position="bottom-center"
-          gutter={10}
-          toastOptions={{
-            duration: 2000,
-          }}
-        >
-          {(t) => (
-            <div
-              style={{
-                opacity: t.visible ? 1 : 0,
-                transform: t.visible ? "translatey(0)" : "translatey(0.75rem)",
-                transition: "all .2s",
-              }}
-            >
-              {resolveValue(t.message, t)}
-            </div>
-          )}
-        </Toaster>
-      </ColorModeProvider>
-    </main>
-  );
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <ClerkProvider>
+            <main className={`${karla.variable} ${inter.variable} font-sans`}>
+                <style jsx global>{`
+                  html {
+                    font-family: ${karla.style.fontFamily};
+                  }
+
+                  #headlessui-portal-root {
+                    font-family: ${inter.style.fontFamily};
+                  }
+                `}</style>
+                <ColorModeProvider>
+                    <ColorModeScript
+                        initialColorMode="system"
+                        key="chakra-ui-no-flash"
+                        storageKey="chakra-ui-color-mode"
+                    />
+                    <Component {...pageProps} />
+                    <Toaster
+                        containerStyle={{
+                            bottom: 40,
+                            left: 20,
+                            right: 20,
+                        }}
+                        position="bottom-center"
+                        gutter={10}
+                        toastOptions={{
+                            duration: 2000,
+                        }}
+                    >
+                        {(t) => (
+                            <div
+                                style={{
+                                    opacity: t.visible ? 1 : 0,
+                                    transform: t.visible ? "translatey(0)" : "translatey(0.75rem)",
+                                    transition: "all .2s",
+                                }}
+                            >
+                                {resolveValue(t.message, t)}
+                            </div>
+                        )}
+                    </Toaster>
+                </ColorModeProvider>
+            </main>
+        </ClerkProvider>
+    );
 }
