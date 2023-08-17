fc3818f2bdd02eb49f1636d78da6a12d2b29e877
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2023-08-17 00:36:42 -0600

    Updating chat routers

--- core/src/server/Services/chatRouter.ts
+++ core/src/server/Services/chatRouter.ts
@@ -1,8 +1,16 @@
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc"; // Adjust the import path as per your project structure
 import { prisma } from "~/server/db"; // Adjust this import too
 import { createTRPCContext } from "../api/trpc";
+import * as z from 'zod';
 
 type TRPCContext = ReturnType<typeof createTRPCContext>;
+
+const createChatInputSchema = z.object({
+    doctorId: z.number(),
+    patientId: z.number(),
+  });
+
+type CreateChatInput = z.infer<typeof createChatInputSchema>;
 
 // Define the procedures
 const allChats = {
@@ -19,9 +27,9 @@ const allChats = {
 
 const getChat = {
     type: "query",
-    input: (z) => z.number(),
-    resolve: async ({ input, ctx }) => {
-        const chat = await ctx.prisma.chat.findUnique({
+    input: z.number(),
+    resolve: async ({ input, ctx }: { input: number, ctx: TRPCContext }) => {
+        const chat = (await ctx).prisma.chat.findUnique({
             where: { id: input },
             include: {
                 messages: true,
@@ -34,30 +42,35 @@ const getChat = {
 
 const createMessage = {
     type: "mutation",
-    input: (z) => z.object({
+    input: z.object({
         chatId: z.number(),
         content: z.string(),
     }),
-    resolve: async ({ input, ctx }) => {
-        const message = await ctx.prisma.message.create({
+    resolve: async ({ input, ctx }: { input: { chatId: number; content: string }, ctx: TRPCContext }) => {
+        const currentUser = (await ctx).currentUser;
+        if (!currentUser || !currentUser.id) {
+            throw new Error("Current user or user ID not found");
+        }
+
+        const senderId = parseInt(currentUser.id, 10);
+
+        const message = (await ctx).prisma.message.create({
             data: {
                 content: input.content,
                 chatId: input.chatId,
-                senderId: ctx.currentUser.id, // Assuming ctx.currentUser contains the authenticated user
+                senderId: senderId, 
             },
         });
         return message;
     },
 };
 
+
 const createChat = {
     type: "mutation",
-    input: (z) => z.object({
-        doctorId: z.number(),
-        patientId: z.number(),
-    }),
-    resolve: async ({ input, ctx }) => {
-        const chat = await ctx.prisma.chat.create({
+    input: createChatInputSchema,
+    resolve: async ({ input, ctx }: { input: CreateChatInput, ctx: TRPCContext }) => {
+        const chat = (await ctx).prisma.chat.create({
             data: {
                 doctorId: input.doctorId,
                 patientId: input.patientId,
@@ -65,7 +78,8 @@ const createChat = {
         });
         return chat;
     },
-};
+  };
+
 
 // Create the router
 export const chatRouter = createTRPCRouter({
