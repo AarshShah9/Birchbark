500836a54cecb0aab0e009d83af30792e984784e
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2023-09-12 21:48:18 -0400

    About us page progress

--- core/src/pages/CustomPages/AboutUsPage/index.tsx
+++ core/src/pages/CustomPages/AboutUsPage/index.tsx
@@ -27,7 +27,7 @@ const AboutUsPage: React.FC = () => {
                 <div className='flex items-center justify-center w-screen h-56  '>
                   <motion.div whileHover={{scale:1.1}}   className=' flex flex-col items-center justify-center h-96 w-[18%] md:w-64 bg-[#303334] shadow-xl rounded-2xl p-4 m-4'>
                     <div>
-                      <img src='/HeartIcon.svg' alt='icon' className='w-12'/>
+                      <img src='/icons/heart.svg' alt='icon' className='w-12'/>
                     </div>
                     <h1 className='font-inikaBold text-5xl font-bold mb-4'>Trust</h1>
                     <p className='font-inikaRegular text-xl'>We are Symptom 360, a group of canadians proudly serving First Nations and Indigenous. We are here to connect you to doctors remotely and get you the help you need.</p>
@@ -35,7 +35,7 @@ const AboutUsPage: React.FC = () => {
 
                   <motion.div whileHover={{scale:1.1}}  className=' flex flex-col items-center justify-center h-96 w-[18%] md:w-64 bg-[#303334] shadow-xl rounded-2xl p-4 m-4'>
                     <div>
-                      <img src='/HandshakeIcon.svg' alt='icon' className='w-12'/>
+                      <img src='/icons/handshake.svg' alt='icon' className='w-12'/>
                     </div>
                     <h1 className='font-inikaBold text-5xl font-bold mb-4'>Partnership</h1>
                     <p className='font-inikaRegular text-xl'>We are Symptom 360, a group of canadians proudly serving First Nations and Indigenous. We are here to connect you to doctors remotely and get you the help you need.</p>
@@ -43,7 +43,7 @@ const AboutUsPage: React.FC = () => {
 
                   <motion.div whileHover={{scale:1.1}}  className=' flex flex-col items-center justify-center h-96 w-[18%] md:w-64 bg-[#303334] shadow-xl rounded-2xl p-4 m-4'>
                     <div>
-                      <img src='/MapleLeafIcon.svg' alt='icon' className='w-12'/>
+                      <img src='/icons/mapleLeaf.svg' alt='icon' className='w-12'/>
                     </div>
                     <h1 className='font-inikaBold text-5xl font-bold mb-4'>Culture</h1>
                     <p className='font-inikaRegular text-xl'>We are Symptom 360, a group of canadians proudly serving First Nations and Indigenous. We are here to connect you to doctors remotely and get you the help you need.</p>
@@ -53,11 +53,13 @@ const AboutUsPage: React.FC = () => {
               </div>
             </div>
           </div>
+
+
           <div className='w-full'>
             <img src='/whiteWave.svg' alt='a white wave graphic' className='w-full -z-1' />
-            <div className='w-full h-[1000px] bg-white pt-64 flex justify-center border border-black'>
-              <div className='flex flex-col items-center border border-red-500'>
-                <h1 className='w-[70%] text-center flex justify-center border text-black text-5xl font-inikaBold'>
+            <div className='w-full h-[850px] bg-white pt-64 flex justify-center ='>
+              <div className='flex flex-col items-center '>
+                <h1 className='w-[70%] text-center flex justify-center text-black text-5xl font-inikaBold pb-8'>
                   You choose how you want to talk to your healthcare team
                 </h1>
 
@@ -65,28 +67,28 @@ const AboutUsPage: React.FC = () => {
                 <div className='flex flex-col '>
                   <div className='flex flex-row mb-8'>
                     <div className='w-20 h-20 rounded-full bg-[#4CA9EE] flex justify-center items-center'>
-                      <img src='/video-solid 1.svg' className=''></img>
+                      <img src='/icons/video.svg' className=''></img>
                     </div>
-                    <span className='flex justify-center items-center text-black text-3xl pl-8'>
+                    <span className='flex justify-center items-center text-black text-3xl pl-8 font-medium'>
                       Video: A video call where you can see your doctor face to face.
                     </span>
                   </div>
 
                   <div className='flex flex-row mb-8'>
                     <div className='w-20 h-20 rounded-full bg-[#4CA9EE] flex justify-center items-center'>
-                      <img src='/video-solid 1.svg' className=''></img>
+                      <img src='/icons/phone.svg' className=''></img>
                     </div>
-                    <span className='flex justify-center items-center text-black text-3xl pl-8'>
-                      Video: A video call where you can see your doctor face to face.
+                    <span className='flex justify-center items-center text-black text-3xl pl-8 font-medium'>
+                    Phone: Just call your doctor and talk about your health over the phone.
                     </span>
                   </div>
 
                   <div className='flex flex-row mb-8'>
                     <div className='w-20 h-20 rounded-full bg-[#4CA9EE] flex justify-center items-center'>
-                      <img src='/video-solid 1.svg' className=''></img>
+                      <img src='/icons/texts.svg' className=''></img>
                     </div>
-                    <span className='flex justify-center items-center text-black text-3xl pl-8'>
-                      Video: A video call where you can see your doctor face to face.
+                    <span className='flex justify-center items-center text-black text-3xl pl-8 font-medium'>
+                    Text: If you are in a hurry, you can quickly send a text to your doctor.
                     </span>
                   </div>
                 </div>
@@ -94,7 +96,54 @@ const AboutUsPage: React.FC = () => {
               
             </div>
           </div>
-         
+        </div>
+
+        {/* Section 1 */}
+        <div className='w-full h-[1200px] flex justify-center'>
+        <div className='px-48 py-24'>
+            <h1 className='text-6xl text-center font-inikaBold mb-12'>How Symptom 360 works</h1>
+            <div className='flex flex-col w-full h-auto '>
+              {/* CARD HOLDER */}
+              <div className='flex flex-row justify-left w-full h-auto border'>
+                <div className='relative w-96 h-64 bg-[#303334] rounded-2xl flex justify-center items-center m-5'>
+                  <p className='text-white text-md py-5 pl-15'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus eum rem, accusamus autem dicta aperiam earum quo, ex quibusdam vel, nesciunt reiciendis ullam tempora pariatur tempore similique culpa libero.
+                  </p>
+                  <span className='w-20 h-20 bg-[#4CA9EE] rounded-full absolute -left-10 text-5xl white font-inikaBold'>
+                    <div className='flex justify-center items-center w-full h-full'>
+                      1
+                    </div>
+                  </span>
+                </div>
+              </div>
+              {/* CARD HOLDER */}
+              <div className='flex flex-row justify-right w-full h-auto'>
+                <div className='relative w-96 h-64 bg-[#303334] rounded-2xl flex justify-center items-center m-5'>
+                  <p className='text-white text-md py-5 pl-15'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus eum rem, accusamus autem dicta aperiam earum quo, ex quibusdam vel, nesciunt reiciendis ullam tempora pariatur tempore similique culpa libero.
+                  </p>
+                  <span className='w-20 h-20 bg-[#4CA9EE] rounded-full absolute -left-10 text-5xl white font-inikaBold'>
+                    <div className='flex justify-center items-center w-full h-full'>
+                      2
+                    </div>
+                  </span>
+                </div>
+              </div>
+              {/* CARD HOLDER */}
+              <div className='flex flex-row justify-left w-full h-auto'>
+                <div className='relative w-96 h-64 bg-[#303334] rounded-2xl flex justify-center items-center m-5'>
+                  <p className='text-white text-md py-5 pl-15'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus eum rem, accusamus autem dicta aperiam earum quo, ex quibusdam vel, nesciunt reiciendis ullam tempora pariatur tempore similique culpa libero.
+                  </p>
+                  <span className='w-20 h-20 bg-[#4CA9EE] rounded-full absolute -left-10 text-5xl white font-inikaBold'>
+                    <div className='flex justify-center items-center w-full h-full'>
+                      3
+                    </div>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* FOOTER */}
