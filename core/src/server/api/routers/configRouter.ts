d2d40823cc866a110408e74151443ef31cb7e3f0
AarshShah9 <aarshshah99@gmail.com>
2023-09-17 19:13:10 -0600

    Cleaned branch rdy for merge

--- core/src/server/api/routers/configRouter.ts
+++
@@ -1,7 +0,0 @@
-import {createTRPCRouter, privateProcedure} from "~/server/api/trpc";
-import {z} from "zod";
-import {encrypt, decrypt} from "~/utils/encryption";
-
-export const configRouter = createTRPCRouter({
-});
-
