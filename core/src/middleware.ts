0fb4b1def02c6df3c8d1a539c931d7be71c6cb61
AarshShah9 <aarshshah99@gmail.com>
2023-10-21 11:16:53 -0600

    Small changed

--- core/src/middleware.ts
+++ core/src/middleware.ts
@@ -1,4 +1,5 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -17,6 +18,16 @@ export default authMiddleware({
     "/pricing",
     "/create-organization",
   ],
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    if (auth.userId && req.nextUrl.pathname === "/sign-in") {
+      const orgSelection = new URL("/app/calendar", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
--- core/src/pages/LandingPages/AboutUsPage/index.tsx
+++ core/src/pages/LandingPages/AboutUsPage/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../../../customComponents/Navbar";
 import Footer from "../../../customComponents/Footer";
 import { motion } from "framer-motion";
-import { aboutUsResources } from "../../../itlText/aboutus";
+import { aboutUsResources } from "~/itlText/aboutus";
 
 const AboutUsPage: React.FC = () => {
   return (
