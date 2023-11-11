07371881926f2b76fd8a31dbacdb40def46c64e9
Eric <85941655+EMedina456@users.noreply.github.com>
2023-11-11 16:19:38 -0500

    Search Bar backend integration

--- core/src/pages/app/articles/category.tsx
+++ core/src/pages/app/articles/category.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { FormEvent } from "react";
 import Link from "next/link";
 import { api } from "~/utils/api";
 import Layout from "~/components/Layout";
@@ -39,27 +39,13 @@ const CategoryPage: NextPage = () => {
   return (
     <Layout>
       <div className="relative flex h-[95vh] w-full overflow-auto bg-white font-inter text-[#141718]">
-        <div className="fixed right-14 top-14 z-10 flex w-[20%] rounded-md border-[1px] border-black bg-white hover:shadow-md md:h-9 md:w-44">
-          <input
-            className="w-full rounded-md bg-white p-4 pr-14 md:pr-10"
-            placeholder="Search for an article..."
-          />
-          <div className="group fixed right-14 m-2 outline-none hover:cursor-pointer md:m-0">
-            <Icon
-              className="h-full w-10 fill-black transition-colors group-hover:fill-n-4 md:h-8 md:w-8"
-              name="search-1"
-            />
-          </div>
-        </div>
+        <SearchArticles />
         <div className="flex w-full flex-col items-center">
           <div className="mx-8 flex w-auto flex-col">
             <div className="mb-8 mt-10 flex w-full flex-col items-center">
               <h1 className="flex max-w-md justify-center text-center text-4xl font-bold">
                 Articles in this Category
               </h1>
-              {/* <h2 className="flex justify-center text-center text-2xl text-[#6C7275]">
-                View Articles based on Categories
-              </h2> */}
             </div>
             <ul className="mb-10 grid w-auto grid-cols-2 gap-6 text-center md:grid-cols-1">
               {articles.map((article) => (
@@ -78,14 +64,12 @@ const CategoryPage: NextPage = () => {
                       className="flex flex-row items-center space-x-4 rounded-lg border-2 p-4 hover:shadow-md hover:shadow-[#4CA9EE]"
                     >
                       <Icon
-                        className="h-full w-10 fill-black transition-colors group-hover:fill-n-4"
+                        className="h-full w-8 fill-black transition-colors group-hover:fill-n-4"
                         name="search-1"
                       />
-                      <h1 className="text-2xl font-semibold">
-                        {article.title}
-                      </h1>
+                      <h1 className="text-xl font-semibold">{article.title}</h1>
                       <div className="pl-[8vw] xl:pl-6">
-                        <BsArrowRight size={40} />
+                        <BsArrowRight size={30} />
                       </div>
                     </motion.div>
                   </Link>
@@ -96,6 +80,66 @@ const CategoryPage: NextPage = () => {
         </div>
       </div>
     </Layout>
+  );
+};
+
+const SearchArticles: React.FC = () => {
+  // Replace with the appropriate tRPC hook
+  const [searchVal, setSearchVal] = React.useState("");
+  const [currentSearch, setSearch] = React.useState(searchVal);
+  const { data, error, isLoading } = api.wiki.searchArticlesByTitle.useQuery({
+    title: searchVal,
+  });
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearchVal(currentSearch);
+
+    // Trigger the tRPC query with the input title
+    // You may need to adjust this based on your tRPC hooks setup
+  };
+
+  return (
+    <div className="fixed right-14 z-10 m-10 flex w-80 flex-col bg-white">
+      <form onSubmit={onSubmit}>
+        <div className="flex w-full rounded-md rounded-b-none border-[1px] border-gray-200 hover:shadow-md md:h-9">
+          <input
+            className="z-100 w-full rounded-md rounded-b-none bg-white p-4 pr-14 focus:outline-none md:pr-10"
+            placeholder="Search for an article..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="group fixed right-24 m-2 outline-none hover:cursor-pointer md:m-0"
+          >
+            <Icon
+              className="h-full w-10 fill-gray-600 transition-colors group-hover:fill-n-4 md:h-8 md:w-8"
+              name="search-1"
+            />
+          </button>
+        </div>
+      </form>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && searchVal != "" && (
+        <ul className="flex w-full justify-start border-[1px] border-gray-200">
+          {data.map((article) => (
+            <li
+              className="flex max-h-18 w-full justify-start text-ellipsis hover:shadow-md"
+              key={article.id}
+            >
+              <a
+                className="mx-4 flex w-full flex-col text-ellipsis"
+                href={`/app/articles/article?id=${article.id}`}
+              >
+                <p className="truncate">{article.title}</p>
+                <p className="truncate text-[#6C7275]">{article.description}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
