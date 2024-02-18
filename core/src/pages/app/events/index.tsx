290851039b7c598f013586e220a95757e9a0bb5f
AarshShah9 <aarshshah99@gmail.com>
2024-02-18 17:36:55 -0600

    removed events router

--- core/src/pages/app/events/index.tsx
+++ core/src/pages/app/events/index.tsx
@@ -1,35 +1,10 @@
-import { api } from "~/utils/api";
-import Link from "next/link";
 import React from "react";
 import Layout from "~/components/Layout";
 
 const EventsMain: React.FC = () => {
-  const { data, error, isLoading } = api.events.getAllUpcomingEvents.useQuery();
-  console.log("EVENT DATA:\n" + data);
-
   return (
     <Layout>
-      <div>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
-        {data && (
-          <ul>
-            {data.map((event) => (
-              <li key={event.id}>
-                <Link href={`app/events?eventId=${event.id}`}>
-                  <a>
-                    <h3>{event.title}</h3>
-                    <p>Date: {new Date(event.date).toLocaleDateString()}</p>
-                    {event.imageUrl && (
-                      <img src={event.imageUrl} alt={event.title} />
-                    )}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      <></>
     </Layout>
   );
 };
